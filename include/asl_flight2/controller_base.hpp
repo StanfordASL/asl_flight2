4e251414a11e0f035f3b1af2681175b2789be127
spenrich <spenrich@stanford.edu>
2022-03-12 22:40:09 -0800

    some cleanup, started doxygen comments

--- include/asl_flight2/controller_base.hpp
+++ include/asl_flight2/controller_base.hpp
@@ -8,54 +8,112 @@
 #include <px4_msgs/msg/vehicle_local_position.hpp>
 #include <px4_msgs/msg/vehicle_attitude.hpp>
 #include <px4_msgs/msg/vehicle_angular_velocity.hpp>
+// #include <px4_msgs/msg/actuator_direct_control.hpp>
 
 
-const Eigen::Quaterniond ROT_FRD_FLU = 
-    px4_ros_com::frame_transforms::utils::quaternion::quaternion_from_euler(-M_PI, 0.0, 0.0);
-const Eigen::Quaterniond ROT_NWU_NED = 
-    px4_ros_com::frame_transforms::utils::quaternion::quaternion_from_euler(M_PI, 0.0, 0.0);
+const Eigen::Quaterniond ROT_FRD_FLU
+    = px4_ros_com::frame_transforms::utils::quaternion::quaternion_from_euler(-M_PI, 0.0, 0.0);
 
-void transform_flip_z(const Eigen::Vector3d& vec_in, Eigen::Vector3d& vec_out);
+const Eigen::Quaterniond ROT_NWU_NED
+    = px4_ros_com::frame_transforms::utils::quaternion::quaternion_from_euler(M_PI, 0.0, 0.0);
 
+/**
+ * @brief      { function_description }
+ *
+ * @param[in]  v     { parameter_description }
+ *
+ * @return     { description_of_the_return_value }
+ */
+inline Eigen::Vector3d transform_flip_z(const Eigen::Vector3d& v) {
+    return {v(0), -v(1), -v(2)};
+}
+
+/**
+ * @brief      This class describes a controller base.
+ */
 class ControllerBase : public rclcpp::Node {
-	public:
-		explicit ControllerBase(const std::string& node_name);
-    	virtual ~ControllerBase();
+public:
+    /**
+     * @brief      Constructs a new instance.
+     *
+     * @param[in]  node_name  The node name
+     */
+	explicit ControllerBase(const std::string& node_name, const size_t qos_history_depth_ = 10);
 
-	protected:
-        double loop_freq_;          // control loop frequency (Hz)
+    /**
+     * @brief      Destroys the object.
+     */
+	virtual ~ControllerBase() = default;
 
-        // Frame acronyms:
-        //  NED -- North-East-Down
-        //  NWD -- North-West-Down
-        //  FRD -- Forward-Right-Down
-        //  FLU -- Forward-Left-Up
+protected:
+    double loop_freq_;          // control loop frequency (Hz)
 
-        // Position
-        Eigen::Vector3d pos_ned_;   // position in inertial NED frame
-        Eigen::Vector3d pos_nwu_;   // position in inertial NWU frame
+    // Frame acronyms:
+    //  NED -- North-East-Down
+    //  NWD -- North-West-Down
+    //  FRD -- Forward-Right-Down
+    //  FLU -- Forward-Left-Up
 
-        // Linear velocity
-        Eigen::Vector3d vel_ned_;   // velocity in inertial NED frame
-        Eigen::Vector3d vel_nwu_;   // velocity in inertial NWU frame
-        Eigen::Vector3d vel_frd_;   // velocity in body FRD frame
-        Eigen::Vector3d vel_flu_;   // velocity in body FLU frame
-        
-        // Rotation (NOTE: Eigen quaternions are stored as [x, y, z, w])
-        Eigen::Quaterniond rot_ned_frd_;
-        Eigen::Quaterniond rot_nwu_flu_;
+    // Position
+    Eigen::Vector3d pos_ned_;   // position in inertial NED frame
+    Eigen::Vector3d pos_nwu_;   // position in inertial NWU frame
 
-        // Angular velocity (FLU w.r.t. NWU, and FRD w.r.t. NED)
-        Eigen::Vector3d angvel_frd_;
-        Eigen::Vector3d angvel_flu_;
+    // Linear velocity
+    Eigen::Vector3d vel_ned_;   // velocity in inertial NED frame
+    Eigen::Vector3d vel_nwu_;   // velocity in inertial NWU frame
+    Eigen::Vector3d vel_frd_;   // velocity in body FRD frame
+    Eigen::Vector3d vel_flu_;   // velocity in body FLU frame
 
-        // Timers
-        rclcpp::TimerBase::SharedPtr timer_;
+    // Attitude (NOTE: Eigen quaternions are stored as [x, y, z, w])
+    Eigen::Quaterniond att_ned_frd_;
+    Eigen::Quaterniond att_nwu_flu_;
 
-        // Callbacks
-        virtual void callbackTimer();
-        virtual void callbackPositionAndVelocity(const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg);
-        virtual void callbackAttitude(const px4_msgs::msg::VehicleAttitude::SharedPtr msg);
-        virtual void callbackAngularVelocity(const px4_msgs::msg::VehicleAngularVelocity::SharedPtr msg);
-        virtual void callbackTimeSync(const px4_msgs::msg::Timesync::UniquePtr msg);
+    // Angular velocity (FLU w.r.t. NWU, and FRD w.r.t. NED)
+    Eigen::Vector3d angvel_flu_;
+    Eigen::Vector3d angvel_frd_;
+
+    // Timers
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    // PX4 message subscriptions
+    rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr sub_pos_vel_;
+    rclcpp::Subscription<px4_msgs::msg::VehicleAttitude>::SharedPtr sub_att_;
+    rclcpp::Subscription<px4_msgs::msg::VehicleAngularVelocity>::SharedPtr sub_angvel_;
+    rclcpp::Subscription<px4_msgs::msg::Timesync>::SharedPtr sub_timesync_;
+
+
+    // Callbacks
+
+    /**
+     * @brief      { function_description }
+     */
+    virtual void callbackTimer();
+
+    /**
+     * @brief      { function_description }
+     *
+     * @param[in]  msg   The message
+     */
+    virtual void callbackPositionAndVelocity(const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg);
+
+    /**
+     * @brief      { function_description }
+     *
+     * @param[in]  msg   The message
+     */
+    virtual void callbackAttitude(const px4_msgs::msg::VehicleAttitude::SharedPtr msg);
+
+    /**
+     * @brief      { function_description }
+     *
+     * @param[in]  msg   The message
+     */
+    virtual void callbackAngularVelocity(const px4_msgs::msg::VehicleAngularVelocity::SharedPtr msg);
+
+    /**
+     * @brief      { function_description }
+     *
+     * @param[in]  msg   The message
+     */
+    virtual void callbackTimeSync(const px4_msgs::msg::Timesync::UniquePtr msg);
 };
--- src/controller_base.cpp
+++ src/controller_base.cpp
@@ -1,54 +1,60 @@
 #include "asl_flight2/controller_base.hpp"
 
-void transform_flip_z(const Eigen::Vector3d& vec_in, Eigen::Vector3d& vec_out) {
-    vec_out(0) = vec_in(0);
-    vec_out(1) = -vec_in(1);
-    vec_out(2) = -vec_in(2);
-}
 
-ControllerBase::ControllerBase(const std::string& node_name) : 
-    rclcpp::Node(node_name) 
-{
-    // TODO.
-}
+ControllerBase::ControllerBase(const std::string& node_name, const size_t qos_history_depth_)
+    : rclcpp::Node(node_name) {
+    // Create PX4 message subscriptions
+    // TODO: understand ROS2 QoS profiles better
+    sub_pos_vel_ = create_subscription<px4_msgs::msg::VehicleLocalPosition>(
+        "position_and_velocity",
+        qos_history_depth_,
+        std::bind(&ControllerBase::callbackPositionAndVelocity, this, std::placeholders::_1)
+    );
 
-ControllerBase::~ControllerBase() {};
+    sub_att_ = create_subscription<px4_msgs::msg::VehicleAttitude>(
+        "attitude",
+        qos_history_depth_,
+        std::bind(&ControllerBase::callbackAttitude, this, std::placeholders::_1)
+    );
+
+    sub_angvel_ = create_subscription<px4_msgs::msg::VehicleAngularVelocity>(
+        "angular_velocity",
+        qos_history_depth_,
+        std::bind(&ControllerBase::callbackAngularVelocity, this, std::placeholders::_1)
+    );
+
+    sub_timesync_ = create_subscription<px4_msgs::msg::Timesync>(
+        "time_sync",
+        qos_history_depth_,
+        std::bind(&ControllerBase::callbackTimeSync, this, std::placeholders::_1)
+    );
+};
+
 
 void ControllerBase::callbackTimer() {
     RCLCPP_INFO(this->get_logger(), "Hello from ROS2");
 }
 
 void ControllerBase::callbackPositionAndVelocity(const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg) {
-    pos_ned_(0) = msg->x;
-    pos_ned_(1) = msg->y;
-    pos_ned_(2) = msg->z;
-    transform_flip_z(pos_ned_, pos_nwu_);
-
-    vel_ned_(0) = msg->vx;
-    vel_ned_(1) = msg->vy;
-    vel_ned_(2) = msg->vz;
-    transform_flip_z(vel_ned_, vel_nwu_);
-
-    vel_frd_ = rot_ned_frd_.conjugate() * vel_ned_;
-    transform_flip_z(vel_frd_, vel_flu_);
-
+    pos_ned_ = {msg->x, msg->y, msg->z};
+    vel_ned_ = {msg->vx, msg->vy, msg->vz};
+    pos_nwu_ = transform_flip_z(pos_ned_);
+    vel_nwu_ = transform_flip_z(vel_ned_);
+    vel_frd_ = att_ned_frd_.conjugate() * vel_ned_;
+    vel_flu_ = transform_flip_z(vel_frd_);
     // timestamp_.store(msg->timestamp);
 }
 
 void ControllerBase::callbackAttitude(const px4_msgs::msg::VehicleAttitude::SharedPtr msg) {
-    rot_ned_frd_ = px4_ros_com::frame_transforms::utils::quaternion::array_to_eigen_quat(msg->q);
-    rot_ned_frd_.normalize();
-    rot_nwu_flu_ = ROT_NWU_NED * rot_ned_frd_ * ROT_FRD_FLU;
-
+    att_ned_frd_ = px4_ros_com::frame_transforms::utils::quaternion::array_to_eigen_quat(msg->q);
+    att_ned_frd_.normalize();
+    att_nwu_flu_ = ROT_NWU_NED * att_ned_frd_ * ROT_FRD_FLU;
     // timestamp_.store(msg->timestamp);
 }
 
 void ControllerBase::callbackAngularVelocity(const px4_msgs::msg::VehicleAngularVelocity::SharedPtr msg) {
-    angvel_frd_(0) = msg->xyz;
-    angvel_frd_(1) = msg->xyz;
-    angvel_frd_(2) = msg->xyz;
-    transform_flip_z(angvel_frd_, angvel_flu_);
-
+    angvel_frd_ = {msg->xyz[0], msg->xyz[1], msg->xyz[2]};
+    angvel_flu_ = transform_flip_z(angvel_frd_);
     // timestamp_.store(msg->timestamp);
 }
 
