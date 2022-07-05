56c869234bd5528c5cde7039aff6203c23b0a4d2
Alvin Sun <alvinsunyixiao@gmail.com>
2022-07-05 16:29:25 -0700

    clean up

--- include/asl_flight2/controller_base.hpp
+++ include/asl_flight2/controller_base.hpp
@@ -3,118 +3,75 @@
 #include <atomic>
 
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
 #include <rclcpp/rclcpp.hpp>
 
-#include <px4_ros_com/frame_transforms.h>
 #include <px4_msgs/msg/timesync.hpp>
-#include <px4_msgs/msg/vehicle_local_position.hpp>
-#include <px4_msgs/msg/vehicle_attitude.hpp>
-#include <px4_msgs/msg/vehicle_angular_velocity.hpp>
 #include <px4_msgs/msg/vehicle_odometry.hpp>
 
+#include <geometry_msgs/msg/pose_stamped.hpp>
 
-const Eigen::Quaterniond ROT_FRD_FLU
-    = px4_ros_com::frame_transforms::utils::quaternion::quaternion_from_euler(-M_PI, 0.0, 0.0);
 
-const Eigen::Quaterniond ROT_NWU_NED
-    = px4_ros_com::frame_transforms::utils::quaternion::quaternion_from_euler(M_PI, 0.0, 0.0);
-
-/**
- * @brief      { function_description }
- *
- * @param[in]  v     { parameter_description }
- *
- * @return     { description_of_the_return_value }
- */
-inline Eigen::Vector3d transform_flip_z(const Eigen::Vector3d& v) {
-    return {v(0), -v(1), -v(2)};
-}
+namespace asl {
 
 /**
  * @brief      This class describes a controller base.
  */
 class ControllerBase : public rclcpp::Node {
-public:
-    /**
-     * @brief      Constructs a new instance.
-     *
-     * @param[in]  node_name  The node name
-     */
-	explicit ControllerBase(const std::string& node_name, const size_t qos_history_depth_ = 10);
+ public:
+  /**
+   * @brief      Constructs a new instance.
+   *
+   * @param[in]  node_name  The node name
+   */
+  explicit ControllerBase(const std::string& node_name, const size_t qos_history_depth = 10);
 
-    /**
-     * @brief      Destroys the object.
-     */
-	virtual ~ControllerBase() = default;
+  /**
+   * @brief      Destroys the object.
+   */
+  virtual ~ControllerBase() = default;
 
-protected:
-    double loop_freq_;          // control loop frequency (Hz)
+ protected:
+  // working in ROS default frame NWU -- North-West-Up
 
-    // Frame acronyms:
-    //  NED -- North-East-Down
-    //  NWD -- North-West-Down
-    //  FRD -- Forward-Right-Down
-    //  FLU -- Forward-Left-Up
+  // position
+  mutable Eigen::Vector3d world_t_body_; // pose position
 
-    // Position
-    mutable Eigen::Vector3d pos_ned_;   // position in inertial NED frame
-    mutable Eigen::Vector3d pos_nwu_;   // position in inertial NWU frame
+  // linear velocity
+  mutable Eigen::Vector3d v_world_; // velocity in inertial frame
+  mutable Eigen::Vector3d v_body_;  // velocity in inertial NED frame
 
-    // Linear velocity
-    mutable Eigen::Vector3d vel_ned_;   // velocity in inertial NED frame
-    mutable Eigen::Vector3d vel_nwu_;   // velocity in inertial NWU frame
-    mutable Eigen::Vector3d vel_frd_;   // velocity in body FRD frame
-    mutable Eigen::Vector3d vel_flu_;   // velocity in body FLU frame
+  // orientation
+  mutable Eigen::Quaterniond world_R_body_;
 
-    // Attitude (NOTE: Eigen quaternions are stored as [x, y, z, w])
-    mutable Eigen::Quaterniond att_ned_frd_;
-    mutable Eigen::Quaterniond att_nwu_flu_;
+  // angular velocity
+  mutable Eigen::Vector3d w_world_;
+  mutable Eigen::Vector3d w_body_;
 
-    // Angular velocity (FLU w.r.t. NWU, and FRD w.r.t. NED)
-    mutable Eigen::Vector3d angvel_flu_;
-    mutable Eigen::Vector3d angvel_frd_;
+  // Synced Timestamp
+  mutable std::atomic<uint64_t> timestamp_synced_;
 
-    // Synced Timestamp
-    mutable std::atomic<uint64_t> timestamp_;
+  // PX4 message subscriptions
+  const rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr sub_odom_;
+  const rclcpp::Subscription<px4_msgs::msg::Timesync>::SharedPtr sub_timesync_;
 
-    // PX4 message subscriptions
-    const rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr sub_odom_;
-    const rclcpp::Subscription<px4_msgs::msg::Timesync>::SharedPtr sub_timesync_;
+  const rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub_;
 
-    // Callbacks
+  // Callbacks
 
-    /**
-     * @brief callback handler for reading state from the flight controller
-     *
-     * @param msg message containing odometry data
-     */
-    virtual void VehicleOdometryCallback(const px4_msgs::msg::VehicleOdometry::SharedPtr msg);
+  /**
+   * @brief callback handler for reading state from the flight controller
+   *
+   * @param msg message containing odometry data
+   */
+  void VehicleOdometryCallback(const px4_msgs::msg::VehicleOdometry::SharedPtr msg) const;
 
-    /**
-     * @brief      { function_description }
-     *
-     * @param[in]  msg   The message
-     */
-    virtual void callbackPositionAndVelocity(const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg);
-
-    /**
-     * @brief      { function_description }
-     *
-     * @param[in]  msg   The message
-     */
-    virtual void callbackAttitude(const px4_msgs::msg::VehicleAttitude::SharedPtr msg);
-
-    /**
-     * @brief      { function_description }
-     *
-     * @param[in]  msg   The message
-     */
-    virtual void callbackAngularVelocity(const px4_msgs::msg::VehicleAngularVelocity::SharedPtr msg);
-
-    /**
-     * @brief      { function_description }
-     *
-     * @param[in]  msg   The message
-     */
-    virtual void TimeSyncCallback(const px4_msgs::msg::Timesync::UniquePtr msg) const;
+   /**
+   * @brief      { function_description }
+   *
+   * @param[in]  msg   The message
+   */
+  void TimeSyncCallback(const px4_msgs::msg::Timesync::SharedPtr msg) const;
 };
+
+} // namespace asl
--- src/controller_base.cpp
+++ src/controller_base.cpp
@@ -3,53 +3,75 @@
 #include <chrono>
 #include <functional>
 
+#include <px4_ros_com/frame_transforms.h>
+#include <tf2_eigen/tf2_eigen.h>
+
+namespace asl {
+
 using namespace std::chrono_literals;
+using namespace px4_msgs::msg;
+using namespace geometry_msgs::msg;
 using std::placeholders::_1;
 
-ControllerBase::ControllerBase(const std::string& node_name, const size_t qos_history_depth_)
+const Eigen::Quaterniond nwu_R_ned(Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX()));
+
+// TODO: understand ROS2 QoS profiles better
+ControllerBase::ControllerBase(const std::string& node_name, const size_t qos_history_depth)
     : rclcpp::Node(node_name),
-      sub_odom_(create_subscription<px4_msgs::msg::VehicleOdometry>(
+      sub_odom_(create_subscription<VehicleOdometry>(
         "/fmu/vehicle_odometry/out",
-        qos_history_depth_,
+        qos_history_depth,
         std::bind(&ControllerBase::VehicleOdometryCallback, this, _1)
       )),
-      sub_timesync_(create_subscription<px4_msgs::msg::Timesync>(
+      sub_timesync_(create_subscription<Timesync>(
         "/fmu/timesync/out",
-        qos_history_depth_,
+        qos_history_depth,
         std::bind(&ControllerBase::TimeSyncCallback, this, _1)
-      )) {
-    // Create PX4 message subscriptions
-    // TODO: understand ROS2 QoS profiles better
+      )),
+      pose_pub_(create_publisher<PoseStamped>("pose", qos_history_depth)) {
 }
 
-void ControllerBase::callbackPositionAndVelocity(const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg) {
-    pos_ned_ = {msg->x, msg->y, msg->z};
-    vel_ned_ = {msg->vx, msg->vy, msg->vz};
-    pos_nwu_ = transform_flip_z(pos_ned_);
-    vel_nwu_ = transform_flip_z(vel_ned_);
-    vel_frd_ = att_ned_frd_.conjugate() * vel_ned_;
-    vel_flu_ = transform_flip_z(vel_frd_);
+void ControllerBase::VehicleOdometryCallback(const VehicleOdometry::SharedPtr msg) const {
+  // position
+  const Eigen::Vector3d world_t_body_ned(msg->x, msg->y, msg->z);
+  world_t_body_ = nwu_R_ned * world_t_body_ned;
+
+  // orientation
+  const Eigen::Quaterniond world_R_body_ned =
+    px4_ros_com::frame_transforms::utils::quaternion::array_to_eigen_quat(msg->q);
+  world_R_body_ = nwu_R_ned * world_R_body_ned.normalized() * nwu_R_ned.conjugate();
+
+  // velocity
+  const Eigen::Vector3d v_body_ned(msg->vx, msg->vy, msg->vz);
+  v_body_ = nwu_R_ned * v_body_ned;
+  v_world_ = world_R_body_ * v_body_;
+
+  // anguler velocity
+  const Eigen::Vector3d w_body_ned(msg->rollspeed, msg->pitchspeed, msg->yawspeed);
+  w_body_ = nwu_R_ned * w_body_ned;
+  w_world_ = world_R_body_ * w_body_;
+
+  // re-publish pose for rviz visualization
+  PoseStamped pose_msg{};
+  pose_msg.header.frame_id = "world";
+  pose_msg.header.stamp = rclcpp::Clock().now();
+  pose_msg.pose.position = tf2::toMsg(world_t_body_);
+  pose_msg.pose.orientation = tf2::toMsg(world_R_body_);
+  pose_pub_->publish(pose_msg);
 }
 
-void ControllerBase::callbackAttitude(const px4_msgs::msg::VehicleAttitude::SharedPtr msg) {
-    att_ned_frd_ = px4_ros_com::frame_transforms::utils::quaternion::array_to_eigen_quat(msg->q);
-    att_ned_frd_.normalize();
-    att_nwu_flu_ = ROT_NWU_NED * att_ned_frd_ * ROT_FRD_FLU;
+void ControllerBase::TimeSyncCallback(const Timesync::SharedPtr msg) const {
+  const uint64_t time_sync = msg->timestamp;
+  const uint64_t time_now = rclcpp::Clock().now().nanoseconds() / 1e3;
+  timestamp_synced_.store(msg->timestamp);
+  RCLCPP_INFO(this->get_logger(), "%lu %lu %ld", time_sync, time_now, (int64_t)time_now - time_sync);
 }
 
-void ControllerBase::callbackAngularVelocity(const px4_msgs::msg::VehicleAngularVelocity::SharedPtr msg) {
-    angvel_frd_ = {msg->xyz[0], msg->xyz[1], msg->xyz[2]};
-    angvel_flu_ = transform_flip_z(angvel_frd_);
-}
-
-void ControllerBase::TimeSyncCallback(const px4_msgs::msg::Timesync::UniquePtr msg) const {
-    timestamp_.store(msg->timestamp);
-}
-
+} // namespace asl
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ControllerBase>("controller_base"));
+    rclcpp::spin(std::make_shared<asl::ControllerBase>("controller_base"));
     rclcpp::shutdown();
     return 0;
 }
