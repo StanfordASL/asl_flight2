af5338cf5d630d9ce84652b1e4a4c441038b1bb7
Alvin Sun <alvinsunyixiao@gmail.com>
2022-07-06 17:02:02 -0700

    add new simple controller

---
+++ include/asl_flight2/simple_controller.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "asl_flight2/controller_base.hpp"
+
+namespace asl {
+
+class SimpleController : public ControllerBase {
+ public:
+  explicit SimpleController(const std::string& node_name);
+
+ protected:
+  mutable Eigen::Vector3d position_setpoint_;
+  mutable double yaw_setpoint_;
+
+  void TakeOff(const double& height, const std::chrono::seconds& duration);
+  void Land(const std::chrono::seconds& duration);
+
+ private:
+  const rclcpp::TimerBase::SharedPtr publish_loop_timer_;
+
+  void SetpointPublishLoop() const;
+};
+
+} // namespace asl
